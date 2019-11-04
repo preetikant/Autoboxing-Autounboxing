97dcf37b57d2c3ca1c8d9ecf6b94e90fd692a8c2
Preeti Kant <44994724+preetikant@users.noreply.github.com>
2019-11-04 15:14:42 +0530

    Add files via upload

---
+++ Autoboxing & Autounboxing/Example of autoboxing & autounboxing/Test1.java
@@ -0,0 +1,7 @@
+class Test1{
+	static Integer I=10;
+	public static void main(String args[]){
+		int m=I;
+		System.out.println(m);
+	}
+}
---
+++ Autoboxing & Autounboxing/Test3.java
@@ -0,0 +1,10 @@
+class Test3{
+	public static void main(String args[]){
+	Integer X=10;
+	Integer Y=X;
+	X++;
+	System.out.println(X);
+	System.out.println(Y);
+	System.out.println(Y==X);
+	}
+}
